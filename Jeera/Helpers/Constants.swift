ef51c3fbb916334ff0e04255c0f1a982d98cd33a
anggidast <adastariana@gmail.com>
2022-10-25 12:10:45 +0700

    feat(main-page/init): add main page

---
+++ Jeera/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+ //  General.swift
+ //  Jeera
+ //
+ //  Created by Anggi Dastariana on 24/10/22.
+ //
+
+ import Foundation
+ import CoreLocation
+
+ let centerCoordinate = CLLocationCoordinate2D(latitude: -6.309337, longitude: 106.821482)
+ let activeStyleURI = "mapbox://styles/anggidast/cl94adatq002114mo8gfbc1qx"
+ let inactiveStyleURI = "mapbox://styles/anggidast/cl9ldvs6j001k14prjtv8su72"
+ let layerStyleIds = ["ragunan-cages", "ragunan-facilities"]
