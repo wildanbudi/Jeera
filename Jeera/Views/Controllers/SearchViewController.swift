ca9a1a72a17a7e5d7dc36eb0d169eb722ba58776
anggidast <adastariana@gmail.com>
2022-11-03 21:50:34 +0700

    feat(search-modal): add table view delegate

--- Jeera/Views/Controllers/SearchViewController.swift
+++ Jeera/Views/Controllers/SearchViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        <#code#>
+//    }
     
     var animalsData: [Animals]!
     var cagesData: [Cages]!
     var facilitiesData: [Facilities]!
+    var searchResults: [Any] = []
+    let tableView = UITableView()
     
     lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
@@ -35,11 +40,36 @@ class SearchViewController: UIViewController {
         return sb
     }()
     
+//    lazy var searchController: UISearchController = {
+//        let controller = UISearchController(searchResultsController: nil)
+//        controller.searchResultsUpdater = self
+//        controller.obscuresBackgroundDuringPresentation = false
+//        controller.hidesNavigationBarDuringPresentation = false
+//        controller.searchBar.tintColor = .PrimaryGreen
+//        controller.searchBar.setImage(UIImage(systemName: "magnifyingglass")?.imageWithColor(newColor: .PrimaryGreen), for: .search, state: .normal)
+//        controller.searchBar.placeholder = "Cari Hewan..."
+//        controller.searchBar.becomeFirstResponder()
+//        controller.searchBar.searchTextField.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 1)
+//        controller.searchBar.searchTextField.font = UIFont(name: "Baloo2-Regular", size: 17)
+//        controller.searchBar.searchTextField.layer.cornerRadius = 18
+//        controller.searchBar.searchTextField.clipsToBounds = true
+//        controller.searchBar.searchTextField.backgroundColor = .white
+//        controller.searchBar.searchTextField.layer.borderColor = UIColor.PrimaryGreen.cgColor
+//        controller.searchBar.searchTextField.layer.borderWidth = 1
+//        controller.searchBar.backgroundImage = UIImage()
+//
+//        return controller
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(searchBar)
-        print(animalsData.count, cagesData.count, facilitiesData.count)
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        //        print(animalsData.count, cagesData.count, facilitiesData.count)
         
         searchBar.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
@@ -50,7 +80,28 @@ class SearchViewController: UIViewController {
             paddingRight: 16,
             height: 36
         )
+        
+        tableView.anchor(
+            top: searchBar.bottomAnchor,
+            left: view.leftAnchor,
+            right: view.rightAnchor,
+            paddingTop: 10,
+            height: view.bounds.height
+        )
+        
+//        tableView.tableHeaderView = searchController.searchBar
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        tableView.anchor(
+//            top: searchBar.bottomAnchor,
+//            left: view.leftAnchor,
+//            right: view.rightAnchor,
+//            paddingTop: 10,
+//            height: view.bounds.height
+//        )
+//    }
     
     
     /*
@@ -62,7 +113,20 @@ class SearchViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if facilitiesData.count > 0 {
+            return facilitiesData.count
+        } else {
+            return animalsData.count + cagesData.count
+        }
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = animalsData[indexPath.row].idName
+        
+        return cell
+    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
@@ -76,5 +140,6 @@ extension SearchViewController: UISearchBarDelegate {
         } else {
             searchBar.searchTextField.font = UIFont(name: "Baloo2-Regular", size: 17)
         }
+//        searchResults = 
     }
 }
